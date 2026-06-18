546861ffa3895893ac22828a0da5581def1244c3
최종환 <exitms14@BOKNET.OUTRA>
2026-06-18 13:38:13 +0900

    uv 로 관리

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+set POSTGRESQL_USER=suneung
+set POSTGRESQL_PASSWORD=
+set POSTGRESQL_SERVER=newtale.asuscomm.com
+set POSTGRESQL_PORT=5432
+set POSTGRESQL_DB=suneung_math
+
+call python main.py
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set POSTGRESQL_USER=suneung
+set POSTGRESQL_PASSWORD=
+set POSTGRESQL_SERVER=newtale.asuscomm.com
+set POSTGRESQL_PORT=5432
+set POSTGRESQL_DB=suneung_math
+
+python main.py
